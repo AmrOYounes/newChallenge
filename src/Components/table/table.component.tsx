80a1986ed9c6cf50e95975672806678a47c9642a
Amr Younis <ayounis@asaltech.com>
2019-11-26 09:28:18 +0200

    newwork

--- src/Components/table/table.component.tsx
+++ src/Components/table/table.component.tsx
@@ -1,74 +1,220 @@
-import React from 'react';
-import { Panel, PanelBody, PanelFooter, PanelHeader ,TeamsThemeContext,PrimaryButton} from 'msteams-ui-components-react';
-import {Table,TBody,Tr,Th,Td} from 'msteams-ui-components-react';
-import {style} from "typestyle";
-import './table.styles.scss'
-import { MSTeamsIcon, MSTeamsIconType, MSTeamsIconWeight } from 'msteams-ui-icons-react';
-interface Props {}
+import React from "react";
+import {
+  Panel,
+  PanelBody,
+  PanelFooter,
+  PanelHeader,
+  TeamsThemeContext,
+  Dropdown,
+  IconButton
+} from "msteams-ui-components-react";
+import { Table, TBody, Tr, Th, Td } from "msteams-ui-components-react";
+import {
+  MSTeamsIcon,
+  MSTeamsIconType,
+  MSTeamsIconWeight
+} from "msteams-ui-icons-react";
+import "./table.styles.scss";
+interface Props { }
 
 interface State {
-  data: { one: number, two: number, three: number }[],
-  color:['#d69cb5','#d7edd9','#d5ddf7']
-};
+  data: {
+    avatar: string;
+    name: string;
+    type: string;
+    Duration: string;
+    Date: string;
+    three: number;
+  }[];
+}
 class TableExample extends React.Component<Props, State> {
-  constructor(props:any) {
+  constructor(props: any) {
     super(props);
     this.state = {
-      data: [{one: 1,two: 2,three: 3},{one: 1,two: 2,three: 3},{one: 1,two: 2,three: 3},{one: 1,two: 2,three: 3}],
-      color:['#d69cb5','#d7edd9','#d5ddf7']
+      data: [
+        {
+          avatar: "https://ui-avatars.com/api/?rounded=true",
+          name: "Juilen Herrero",
+          type: "Incoming",
+          Duration: "45sec",
+          Date: "29/11 11:43",
+          three: 3
+        },
+        {
+          avatar: "https://ui-avatars.com/api/?rounded=true",
+          name: "Juilen Herrero",
+          type: "Missed call",
+          Duration: "20sec",
+          Date: "22/10 11:43",
+          three: 3
+        },
+        {
+          avatar: "https://ui-avatars.com/api/?rounded=true",
+          name: "Juilen Herrero",
+          type: "Incoming",
+          Duration: "10sec",
+          Date: "11/11 11:43",
+          three: 3
+        },
+        {
+          avatar: "https://ui-avatars.com/api/?rounded=true",
+          name: "Juilen Herrero",
+          type: "Outgoing",
+          Duration: "33sec",
+          Date: "25/12 11:43",
+          three: 3
+        }
+      ],
+
     };
   }
 
-  // <MSTeamsIcon
-  // iconType={MSTeamsIconType.AlertOff}
-  // iconWeight={MSTeamsIconWeight.Light} /> 
-  
+  getCallIcon = (type: string) => {
+    switch (type) {
+      case "Incoming":
+        return (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <MSTeamsIcon
+              iconType={MSTeamsIconType.CallIn}
+              iconWeight={MSTeamsIconWeight.Light}
+            />
+            <span>Incoming</span>
+          </div>
+        );
+        break;
+      case "Outgoing":
+        return (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <MSTeamsIcon
+              iconType={MSTeamsIconType.CallOut}
+              iconWeight={MSTeamsIconWeight.Light}
+            />
+            <span>Outgoing</span>
+          </div>
+        );
+        break;
+      case "Missed call":
+        return (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <MSTeamsIcon
+              style={{ color: "red" }}
+              iconType={MSTeamsIconType.CallMissed}
+              iconWeight={MSTeamsIconWeight.Light}
+            />
+            <span>Missed call</span>
+          </div>
+        );
+    }
+    return <MSTeamsIcon />;
+  };
+
 
   render() {
-    return <TeamsThemeContext.Consumer>
-      {(context) => {
-        const { rem, font } = context;
-        const { sizes, weights } = font;
+    return (
+      <TeamsThemeContext.Consumer>
+        {context => {
+          const { rem, font } = context;
+          const { sizes, weights } = font;
+          const styles = {
+            header: { ...sizes.title, ...weights.semibold },
+            section: {
+              ...sizes.title2,
+              marginTop: rem(1.4),
+              marginBottom: rem(1.4)
+            }
+          };
 
-        const styles = {
-          header: {...sizes.title, ...weights.semibold},
-          section: {...sizes.title2, marginTop: rem(1.4), marginBottom: rem(1.4)},
-        }
+          return (
+            <Panel style={{ width: '50vw', height: '50vh' }} >
+              <PanelHeader>
+                <div style={{ textAlign: 'left', fontWeight: 'bold' }}>Call History </div>
+              </PanelHeader>
+              <PanelBody>
+                <Table className="calls-table">
+                  <TBody>
+                    <Tr>
+                      <Td>Name</Td>
+                      <Td>Type</Td>
+                      <Td>Duration</Td>
+                      <Td>Date</Td>
+                    </Tr>
+                    {this.state.data.map((item, index) => (
+                      <Tr>
+                        <Td>
+                          <img
+                            src="https://ui-avatars.com/api/?rounded=true"
+                            alt="Avatar"
+                            className="avatar"
+                          />
+                          <span>{item.name} </span>
+                        </Td>
+                        <Td>
 
-        return <Panel>
-          <PanelHeader>
-            <div style={styles.header}>Tables</div>
-          </PanelHeader>
-          <PanelBody>
-            <div style={styles.section}>Single Column</div>
-            <Table>
-              <TBody>
-                <Tr><Th>col1</Th></Tr>
-                <Tr><Td>a</Td></Tr>
-                <Tr><Td>1</Td></Tr>
-              </TBody>
-            </Table>
-            <div style={styles.section}>Multiple Columns</div>
-            <Table className='calls-table'>
-              <TBody>
-              <Tr><Th>Name</Th><Th >Type</Th><Th >Duration</Th><Th >Date</Th></Tr>
-                {
-                  this.state.data.map((item,index) => ( 
-                    <Tr><Td> <span style={{backgroundColor:this.state.color[index]}} className='name-icon'>dsf</span>                
-                    {item.one}</Td><Td>{item.two}</Td> <Td>{item.three}</Td><Td>{item.three}</Td></Tr>
-                  ))
-                }
-                {/* <Tr ><Th>col1</Th><Th style={{flex: `0 0 ${rem(3.8)}`}}>col2</Th></Tr>
-                <Tr><Td>a</Td><Td style={{flex: `0 0 ${rem(3.8)}`, textAlign: 'center'}}>b</Td></Tr>
-                <Tr><Td>1</Td><Td style={{flex: `0 0 ${rem(3.8)}`, textAlign: 'center'}}>2</Td></Tr> */}
-              </TBody>
-            </Table>
-          </PanelBody>
-          <PanelFooter>
-          </PanelFooter>
-        </Panel>;
-      }}
-    </TeamsThemeContext.Consumer>
+                          {this.getCallIcon(item.type)}
+                        </Td>
+
+                        <Td>{item.Duration}</Td>
+
+                        <Td>
+                          <span>{item.Date}</span>
+
+                          <IconButton
+                            style={{
+                              background: "white",
+                              color: "#BEBBB8",
+                              borderRadius: "50%",
+                              marginLeft: "1.2vw",
+                              width: "35px",
+                              height: "35px"
+                            }}
+                            iconType={MSTeamsIconType.CallStartBig}
+                          />
+
+                          <Dropdown
+                            autoFocus
+                            mainButtonText=""
+                            style={{ position: "absolute", right: "16px", }}
+                            items={[
+                              {
+                                render: () => (
+                                  <div className="dropdown-Content">
+                                    <MSTeamsIcon
+                                      iconWeight={MSTeamsIconWeight.Light}
+                                      iconType={
+                                        MSTeamsIconType.IllustrationMonkey
+                                      }
+                                    />
+                                    <span>Add to speed dial</span>
+                                  </div>
+                                ),
+                                onClick: () => console.log("hello")
+                              },
+                              {
+                                render: () => (
+                                  <div className="dropdown-Content">
+                                    <MSTeamsIcon
+                                      iconWeight={MSTeamsIconWeight.Light}
+                                      iconType={MSTeamsIconType.Delete}
+                                    />
+                                    <span>Delete</span>
+                                  </div>
+                                ),
+                                onClick: () => console.log("hello")
+                              }
+                            ]}
+                          />
+                        </Td>
+                      </Tr>
+                    ))}
+                  </TBody>
+                </Table>
+              </PanelBody>
+              <PanelFooter></PanelFooter>
+            </Panel>
+          );
+        }}
+      </TeamsThemeContext.Consumer>
+    );
   }
 }
 
